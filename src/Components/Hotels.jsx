cde078a3e11d3f4510f23dd8389c80d184e6603f
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-07-24 00:35:54 +0300

    getting data from api in hotels

--- src/Components/Hotels.jsx
+++ src/Components/Hotels.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Hotels.css";
 import { Link } from "react-router-dom";
-import hotel_img from "../assets/hotel-img.jpeg";
+import axios from "axios";
+import { BASE } from "../API/Api";
 import locationIcon from "../assets/location-icon.svg";
 import elipse1 from "../assets/hotel-elipse1.svg";
 import elipse2 from "../assets/hotel-elipse2.svg";
 
 export default function Hotels() {
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE}/hotels`)
+      .then((res) => {
+        setHotels(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <i
+          key={i}
+          className={`fa-solid fa-star mx-1 ${
+            i <= rating ? "text-warning" : "text-secondary"
+          }`}
+        ></i>
+      );
+    }
+    return stars;
+  };
+
   return (
     <div
       className="hotels py-5 position-relative"
@@ -16,7 +45,7 @@ export default function Hotels() {
     >
       <img
         src={elipse1}
-        className="elipse1 position-absolute d-md-block d-none"
+        className="elipse1 position-absolute d-lg-block d-none"
         alt="elipse1"
         style={{
           right: "25%",
@@ -81,111 +110,42 @@ export default function Hotels() {
           </Link>
         </div>
         <div className="content d-flex flex-wrap flex-md-nowrap gap-4 my-3 justify-content-center">
-          <div className="box col-md-4 bg-white p-2 rounded">
-            <div className="image p-2">
-              <img src={hotel_img} className="w-100 rounded" alt="hotel" />
-            </div>
-            <div className="text px-2">
-              <div className="d-flex align-items-center">
-                <h3 className="fw-bold">Sherton</h3>
+          {hotels.slice(0, 3).map((hotel, i) => (
+            <div key={i} className="box col-md-4 bg-white p-2 rounded">
+              <div className="image p-2">
+                <img
+                  src={hotel.images[0].url}
+                  className="w-100 rounded"
+                  alt="hotel"
+                />
+              </div>
+              <div className="text px-2">
+                <div className="d-flex align-items-center">
+                  <h3 className="fw-bold">{hotel.name}</h3>
+                  <p
+                    className="ms-auto fs-2 fw-bold"
+                    style={{
+                      color: "#F77A40",
+                    }}
+                  >
+                    {hotel.ticket_price_from} DH
+                  </p>
+                </div>
                 <p
-                  className="ms-auto fs-2 fw-bold"
                   style={{
-                    color: "#F77A40",
+                    color: "#6B7A85",
                   }}
                 >
-                  45 DH
+                  <img src={locationIcon} className="px-1" alt="location" />
+                  {hotel.location}
                 </p>
-              </div>
-              <p
-                style={{
-                  color: "#6B7A85",
-                }}
-              >
-                <img src={locationIcon} className="px-1" alt="location" />
-                Location, location location
-              </p>
-              <div className="rating">
-                <div>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star mx-1 text-secondary"></i>
+                <div className="rating d-flex fw-bold fs-6 gap-1 align-items-center">
+                  {renderStars(hotel.rating)}
+                  {hotel.rating} ({hotel.number_of_ratings})
                 </div>
               </div>
             </div>
-          </div>
-          <div className="box col-md-4 bg-white p-2 rounded">
-            <div className="image p-2">
-              <img src={hotel_img} className="w-100 rounded" alt="hotel" />
-            </div>
-            <div className="text px-2">
-              <div className="d-flex align-items-center">
-                <h3 className="fw-bold">Sherton</h3>
-                <p
-                  className="ms-auto fs-2 fw-bold"
-                  style={{
-                    color: "#F77A40",
-                  }}
-                >
-                  45 DH
-                </p>
-              </div>
-              <p
-                style={{
-                  color: "#6B7A85",
-                }}
-              >
-                <img src={locationIcon} className="px-1" alt="location" />
-                Location, location location
-              </p>
-              <div className="rating">
-                <div>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star mx-1 text-secondary"></i>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="box col-md-4 bg-white p-2 rounded">
-            <div className="image p-2">
-              <img src={hotel_img} className="w-100 rounded" alt="hotel" />
-            </div>
-            <div className="text px-2">
-              <div className="d-flex align-items-center">
-                <h3 className="fw-bold">Sherton</h3>
-                <p
-                  className="ms-auto fs-2 fw-bold"
-                  style={{
-                    color: "#F77A40",
-                  }}
-                >
-                  45 DH
-                </p>
-              </div>
-              <p
-                style={{
-                  color: "#6B7A85",
-                }}
-              >
-                <img src={locationIcon} className="px-1" alt="location" />
-                Location, location location
-              </p>
-              <div className="rating">
-                <div>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star text-warning mx-1"></i>
-                  <i className="fa-solid fa-star mx-1 text-secondary"></i>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
